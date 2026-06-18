e33065093c1f3590c9dc865658a0e6b3dac184a7
TotallyLumi <totallylumi@proton.me>
2026-06-18 14:12:44 +0100

    refactor: Home page has been remaked

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="px-4 w-full">
-      <header className="h-[70vh] bg-cover bg-center flex items-center text-left justify-center">
-        <div className="bg-black/65 border border-pink-500 p-8 rounded-lg text-left shadow-xl">
-          <h1 className="text-xl md:text-2xl">Hello</h1>
-        </div>
-      </header>
-    </div>
-  );
+  return <>
+    <h1 className="text-xl md:text-2xl">totallylumi.top</h1>
+    <p>
+      Hello there, I'm Lumi and welcome to my website! <br/>
+      I've always had a passion in technology, programming, gaming and music. <br/>
+      I've been programming since I was 12 years old and mainly know HTML, JavaScript/TypeScript and CSharp.
+    </p>
+    <br/>
+    <h1 className="text-xl md:text-2xl">Contact</h1>
+    <p>These are the different ways, you can contact me for any reasons.</p>
+    <p>Discord: <a className="hover:text-zinc-400 transition" href="https://discord.com/users/1257791775523868735">@totallylumi</a> | Alt Account: <a className="hover:text-zinc-400 transition" href="https://discord.com/users/1504915933452570634">@xexblooms</a></p>
+    <p>Email: <a className="hover:text-zinc-400 transition" href="mailto:totallylumi@proton.me">@totallylumi@proton.me</a></p>
+    <p>GitHub: <a className="hover:text-zinc-400 transition" href="https://github.com/totallylumi">@TotallyLumi</a></p>
+  </>
 }
